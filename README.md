d1814b26fcf3512808dbd207184a64a56a3c96ee
Hello-AndyJoe <andrew.joseph.roper@gmail.com>
2023-08-31 21:27:49 -0700

    Added the ability to delete saved notes with app.delete and wrote out README

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 ## Description
 
+This application provides users a place to save notes for later viewing. The use of Express allows for these notes to be saved to a persistenant database which can be accessed anywhere with the use of the application's link. This means the user can have these handy notes ready to use when needed such as work or running errands.
+
 
 ## Table of Contents
 * [Description](#description)
@@ -18,16 +20,22 @@
 
 ## Installation
 
+There's no installation needed, just visit the link []().
+
 
 ## Usage
+
+By visiting the provided link, this application can be used by clicking the "Get Started" button in the middle of the page. This will direct the user to the notes page. Here, the user can input a Note Title and Note Text in the labeled text fields. With information in these field, a save icon will appear in the upper right corner. Clicking this will cause the valued that have been entered into the aforementioned text fields to be saved with the Note Title appearing in a list on the left side of the screen. The user can creat multiple notes as needed. Saved notes can be clicked to view again in the main text field area on the right side of the screen. Notes can also be deleted by clicking the trash icon next to its title.
+
+![Screenshot of Note Taker Application](./assets/express_notetaker_screenshot.png)
 
 
 ## Contributions
 Starting code provided by the Full Stack Bootcamp.
 
-https://www.npmjs.com/package/uuid?activeTab=readme
+Structure of the `app.post` modeled after activity 13-Ins_Data-Persistence in the EXPRESS module of the Full Stack Bootcamp gitlab.
 
-https://github.com/uuidjs/uuid
+The NPM of [uuid](https://www.npmjs.com/package/uuid?activeTab=readme) used for the creation of the random ID value used in the note objects.
 
 
 ## Test
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-// BOILER PLATE CODE 
 const express = require('express');
 const path = require('path');
-const uniqueID = require('uuid');
+const uniqueID = require('uuid'); // NPM Used to generate random ID number for note entry
 const app = express();
 const PORT = 3001;
 const fs = require('fs');
@@ -10,15 +9,17 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Not Needed but it's outlined in the assignment
+//GET route to return the index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+//GET route to return the notes.html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+//POST for the creation of a new note into the note array API
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method}`);
 
@@ -29,6 +30,7 @@ app.post('/api/notes', (req, res) => {
         id: uniqueID.v4()
     }
 
+    // First reads and parses the note array in write to due its nature of being JSON object
     fs.readFile('./db/db.json', (err, data) => {
         if (err) {
             console.error(err);
@@ -36,8 +38,10 @@ app.post('/api/notes', (req, res) => {
             const readNotes = JSON.parse(data);
             readNotes.push(newNote);
 
+            // The writing of the database with the new note having been pushed to the array
             fs.writeFile('./db/db.json', JSON.stringify(readNotes, null, 2), (err) =>
                 err ? console.error(err) :
+                // Reread the note array so when the new note is written, it is displayed
                 fs.readFile('./db/db.json', (err, data) => {
                     if (err) {
                         console.error(err);
@@ -50,12 +54,40 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
+// GET route to read the notes array API
+// Troubleshooted this part with the instructor
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
         if (err) {
             console.error(err);
         } else {
             res.json(JSON.parse(data));
+        }
+    })
+});
+
+// Deletes note from array when its respective trash icon is clicked
+// Worked on this delete portion with tutor
+app.delete('/api/notes/:id', (req, res) => {
+    console.log(req.params.id)
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Create new note array with deleted note filtered out based on ID
+            let alterArray = JSON.parse(data).filter(singleNote => singleNote.id !== req.params.id);
+            // Write over the note array with the altered note array
+            fs.writeFile('./db/db.json', JSON.stringify(alterArray, null, 2), (err) =>
+                err ? console.error(err) :
+                    // Read the new, altered note array
+                    fs.readFile('./db/db.json', (err, data) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            res.json(JSON.parse(data));
+                        }
+                    })
+            );
         }
     })
 });
